a0fca4e0e7237667099b3fee99a44a226fbfbaf5
GigiMZ <133972567+GigiMZ@users.noreply.github.com>
2024-12-11 16:04:33 +0400

    Delete products directory

--- products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=150, default='product name')
-    description = models.TextField(default='')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    create_date = models.DateTimeField(auto_now_add=True)
-    write_time = models.DateTimeField(auto_now=True)
-    views = models.PositiveIntegerField()
